53e73ca275b6e6680c34c7ff2d18b82aebab3232
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2019-11-28 12:23:04 +0600

    layout created

--- App.js
+++ App.js
@@ -7,12 +7,45 @@ export default class App extends Component {
     this.state = {};
   }
   render() {
-    return <View style={styles.container} />;
+    return(
+        <View style={styles.container}>
+          <View style={styles.result}>
+          </View>
+          <View style={styles.calculation}>
+          </View>
+          <View style={styles.buttons}>
+            <View style={styles.numbers}>
+            </View>
+            <View style={styles.operations}>
+            </View>
+          </View>
+        </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  result: {
+    flex: 2,
+    backgroundColor: 'red',
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+  buttons: {
+    flexGrow: 7,
+    flexDirection: 'row',
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: 'yellow',
+  },
+  operations: {
+    flex: 1,
+    backgroundColor: 'black',
   },
 });
